3b60c6c12408778922c4545ab3e5f2280cb475a8
Emily Bowe <ebowe@tcd.ie>
2020-04-08 19:19:14 +0100

    Created DbSingleton class - with private constructor

---
+++ src/com/DesignPatterns/Creational/Singleton/DbSingleton.java
@@ -0,0 +1,10 @@
+package com.DesignPatterns.Creational.Singleton;
+
+public class DbSingleton {
+    private static DbSingleton instance = new DbSingleton();
+    private DbSingleton() {}
+    public static DbSingleton getInstance() {
+        return instance;
+    }
+
+}
