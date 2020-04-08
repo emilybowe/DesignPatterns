f9bd8a0aa1224ea1c00ba6aadfb4bf39c1282519
Emily Bowe <ebowe@tcd.ie>
2020-04-08 20:08:33 +0100

    Implemented Lazy Loading

--- src/com/DesignPatterns/Creational/Singleton/DbSingleton.java
+++ src/com/DesignPatterns/Creational/Singleton/DbSingleton.java
@@ -1,9 +1,12 @@
 package com.DesignPatterns.Creational.Singleton;
 
 public class DbSingleton {
-    private static DbSingleton instance = new DbSingleton();
+    private static DbSingleton instance = null;
     private DbSingleton() {}
     public static DbSingleton getInstance() {
+        if(instance == null) {
+            instance = new DbSingleton();
+        }
         return instance;
     }
 
