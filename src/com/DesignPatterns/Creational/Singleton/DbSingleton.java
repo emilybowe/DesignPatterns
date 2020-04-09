dbe331dfe0724c6101a69eb1ec1977d167a0530e
Emily Bowe <ebowe@tcd.ie>
2020-04-09 12:05:20 +0100

    Implemented Thread Safety - volatile, prevent reflection, synchronization

--- src/com/DesignPatterns/Creational/Singleton/DbSingleton.java
+++ src/com/DesignPatterns/Creational/Singleton/DbSingleton.java
@@ -1,11 +1,20 @@
 package com.DesignPatterns.Creational.Singleton;
 
 public class DbSingleton {
-    private static DbSingleton instance = null;
-    private DbSingleton() {}
+    private static volatile DbSingleton instance = null; //will remain a singleton through any changes in the jvm
+    private DbSingleton() {
+        if(instance != null) { //ensure no-one uses reflection on our code
+            throw new RuntimeException("Use getInstance() method to create");
+        }
+    }
+    //if the whole method is synchronised, it will have a performance cost
     public static DbSingleton getInstance() {
         if(instance == null) {
-            instance = new DbSingleton();
+            synchronized (DbSingleton.class) {
+                if(instance == null) {
+                    instance = new DbSingleton();
+                }
+            }
         }
         return instance;
     }
