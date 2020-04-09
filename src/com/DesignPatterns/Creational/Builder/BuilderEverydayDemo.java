47645a566490c245b868c197685029bf22c3fb0b
Emily Bowe <ebowe@tcd.ie>
2020-04-09 13:48:03 +0100

    Simple Demo of StringBuilder

---
+++ src/com/DesignPatterns/Creational/Builder/BuilderEverydayDemo.java
@@ -0,0 +1,15 @@
+package com.DesignPatterns.Creational.Builder;
+
+public class BuilderEverydayDemo {
+    //faster performance than String Buffer (no locking)
+    public static void main(String[] args) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("This is an example ");
+        builder.append("of the builder pattern. ");
+        builder.append("It has methods to append ");
+        builder.append("almost anything we want to a String. ");
+        builder.append(42);
+        System.out.println(builder);
+
+    }
+}
