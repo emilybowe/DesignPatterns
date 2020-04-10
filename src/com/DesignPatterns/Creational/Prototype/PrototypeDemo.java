a38c73d9411e54ec59117b73e3de0bf8b3e84189
Emily Bowe <ebowe@tcd.ie>
2020-04-10 17:11:53 +0100

    PrototypeDemo class to demo working of classes

---
+++ src/com/DesignPatterns/Creational/Prototype/PrototypeDemo.java
@@ -0,0 +1,19 @@
+package com.DesignPatterns.Creational.Prototype;
+
+public class PrototypeDemo {
+
+    public static void main(String[] args) {
+        Registry registry = new Registry();
+        Movie movie = (Movie) registry.createItem("Movie");
+        movie.setTitle("Creational Patterns in Java");
+
+        System.out.println(movie);
+        System.out.println(movie.getRuntime());
+        System.out.println(movie.getTitle());
+        System.out.println(movie.getUrl());
+
+        Movie anotherMovie = (Movie) registry.createItem("Movie");
+        anotherMovie.setTitle("Gang of four");
+        //unique instance
+    }
+}
