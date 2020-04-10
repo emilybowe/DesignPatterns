14ec7436e181495c3504c786172970d4ed80362e
Emily Bowe <ebowe@tcd.ie>
2020-04-10 13:11:37 +0100

    Add the Book and Movie classes - which extend the Item class

---
+++ src/com/DesignPatterns/Creational/Prototype/Book.java
@@ -0,0 +1,13 @@
+package com.DesignPatterns.Creational.Prototype;
+
+public class Book extends Item {
+    private int numberOfPages;
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+}
