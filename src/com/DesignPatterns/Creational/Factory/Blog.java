42290189975e0f81c4d5cc5e9519d8225cb089b8
Emily Bowe <ebowe@tcd.ie>
2020-04-11 17:38:53 +0100

    Blog extends Website

---
+++ src/com/DesignPatterns/Creational/Factory/Blog.java
@@ -0,0 +1,8 @@
+package com.DesignPatterns.Creational.Factory;
+
+public class Blog extends Website {
+    @Override
+    public void createWebsite() {
+        pages.add(new AboutPage());
+    }
+}
