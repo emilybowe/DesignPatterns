a09dc93c058fe9483c77a23d72d642e35e9298de
Emily Bowe <ebowe@tcd.ie>
2020-04-11 17:58:08 +0100

    FactoryDemo is the main entry point

---
+++ src/com/DesignPatterns/Creational/Factory/FactoryDemo.java
@@ -0,0 +1,10 @@
+package com.DesignPatterns.Creational.Factory;
+
+public class FactoryDemo {
+    public static void main(String[] args) {
+        Website site = WebsiteFactory.getWebsite(WebsiteType.BLOG);
+        System.out.println(site.getPages());
+        site = WebsiteFactory.getWebsite(WebsiteType.SHOP);
+        System.out.println(site.getPages());
+    }
+}
